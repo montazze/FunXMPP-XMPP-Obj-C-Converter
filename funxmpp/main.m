a24e1bcd8ad5747e2c5b80a3e6da941fb32b9e72
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-08-28 14:00:34 +0200

    Initial Commit

---
+++ funxmpp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  funxmpp
+//
+//  Created by Leif Middelschulte on 28.08.12.
+//  Copyright (c) 2012 Leif Middelschulte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
